be590f077b9145db98c95d15b32420fd49e159ef
NataliaBulanova <nbulanova91@gmail.com>
2021-10-14 00:05:32 +0300

    10th task

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,31 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  form: document.querySelector('#controls'),
+  input: document.querySelector('input[type="number"]'),
+  container: document.querySelector('#boxes'),
+  btnCreator: document.querySelector('[data-create]'),
+  btnDestructor: document.querySelector('[data-destroy]'),
+};
+let amount = 0;
+refs.form.addEventListener('input', () => {
+  amount = refs.input.value;
+  refs.btnCreator.addEventListener('click', createBoxes(amount));
+  function createBoxes(nTimes) {
+    const boxMarkup = [];
+    for (let i = 0; i < nTimes; i++) {
+      const box = document.createElement('div');
+      box.style.backgroundColor = getRandomHexColor();
+      box.style.width = `${30 + i * 10}px`;
+      box.style.height = `${30 + i * 10}px`;
+      boxMarkup.push(box);
+    }
+    refs.container.append(...boxMarkup);
+  }
+});
+refs.btnDestructor.addEventListener('click', destroyBoxes);
+function destroyBoxes() {
+  refs.container.innerHTML = '';
+}
